d361f72bc196881e780894312add370d5755908c
zml <zml@aoeu.xyz>
2020-07-10 21:07:03 -0700

    Add sound effects and more bric actions

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 This Minecraft mod adds colourful pride 'bric's to decorate with, wear, and throw at your (fr)enemies.
 Be gay, do crimes!
 
+# Structure
+
+Project source is in `src`
+
+Editor files for assets are in `assets-work`
+
 # License
-This mod is available under LGPLv3.
+The code of this mod is available under LGPLv3.
 See [LICENSE](https://github.com/Queerbric/queerbrics/blob/master/LICENSE) for more details.
+
+Assets are based on works released under a variety of licenses, but all are compattible with CC-BY-SA v4.
+See [LICENSE.MEDIA](https://github.com/Queerbric/queerbrics/blob/master/LICENSE.MEDIA) for more details.
+
--- src/main/java/io/github/queerbric/queerbrics/datatypes/entity/ThrowableBrickEntity.java
+++ src/main/java/io/github/queerbric/queerbrics/datatypes/entity/ThrowableBrickEntity.java
@@ -2,17 +2,26 @@ package io.github.queerbric.queerbrics.datatypes.entity;
 
 import io.github.queerbric.queerbrics.registry.QueerbricsEntityTypes;
 import io.github.queerbric.queerbrics.registry.QueerbricsItems;
+import io.github.queerbric.queerbrics.registry.QueerbricsSoundEvents;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class ThrowableBrickEntity extends ThrownItemEntity {
+
+	private static final int WORLD_EVENT_BREAK_BLOCK = 2001;
 
 	public ThrowableBrickEntity(EntityType<? extends ThrowableBrickEntity> entityType, World world) {
 		super(entityType, world);
@@ -32,18 +41,51 @@ public class ThrowableBrickEntity extends ThrownItemEntity {
 	}
 
 	@Override
-	protected void onEntityHit(EntityHitResult entityHitResult) {
-		super.onEntityHit(entityHitResult);
-		Entity entity = entityHitResult.getEntity();
-		entity.damage(DamageSource.thrownProjectile(this, this.getOwner()),
+	protected void onEntityHit(EntityHitResult collision) {
+		super.onEntityHit(collision);
+		if (!world.isClient) {
+			Entity entity = collision.getEntity();
+			if (entity instanceof LivingEntity) {
+				this.world.playSoundFromEntity(null, entity, QueerbricsSoundEvents.brickHitLiving, SoundCategory.BLOCKS, 1f, 0.8f + this.random.nextFloat() * 0.2f);
+			}
+			entity.damage(DamageSource.thrownProjectile(this, this.getOwner()),
 				(float) Math.sqrt((
-								getVelocity().x * getVelocity().x +
-								getVelocity().y * getVelocity().y +
-								getVelocity().z * getVelocity().z)));
+					getVelocity().x * getVelocity().x +
+						getVelocity().y * getVelocity().y +
+						getVelocity().z * getVelocity().z)));
+		}
+	}
+
+	@Override
+	protected void onBlockHit(BlockHitResult collision) {
+		if (!world.isClient) {
+			BlockPos target = collision.getBlockPos();
+			BlockState state = this.world.getBlockState(target);
+			if (state.getMaterial() == Material.GLASS) {
+				this.world.breakBlock(target, true);
+				return;
+			} else {
+				// play break sound + particles
+				this.world.syncWorldEvent(WORLD_EVENT_BREAK_BLOCK, target, Block.getRawIdFromState(state));
+			}
+			this.destroy();
+		}
+		super.onBlockHit(collision);
 	}
 
 	@Override
 	protected void onCollision(HitResult hitResult) {
 		super.onCollision(hitResult);
+	}
+
+	// Don't make thrown brics disappear
+	@Override
+	public boolean shouldRender(double distance) {
+		return true;
+	}
+
+	@Override
+	protected float getGravity() {
+		return super.getGravity() * 4;
 	}
 }
